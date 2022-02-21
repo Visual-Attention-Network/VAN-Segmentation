80740f8a88924aee91552cbe859e352424621894
MenghaoGuo <38318671+MenghaoGuo@users.noreply.github.com>
2022-02-21 20:00:40 +0800

    Update upernet_van_large_512x512_160k_ade20k.py

--- configs/upernet/VAN/upernet_van_large_512x512_160k_ade20k.py
+++ configs/upernet/VAN/upernet_van_large_512x512_160k_ade20k.py
@@ -3,7 +3,7 @@ _base_ = [
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 model = dict(
-    pretrained='./seg_pretrained/checkpoint-306.pth.tar',
+    pretrained='/path/to/checkpoint',
     backbone=dict(
         type='van_large',
         style='pytorch'),
